dc270f348e7b75843ca8331f0df3101962980b12
BillyPurvis <purvisbilly@outlook.com>
2018-07-02 10:34:42 +0100

    Updates .env-exmple with better mock data

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/BillyPurvis/boommessaging-go/handler"
 	_ "github.com/joho/godotenv/autoload"
@@ -12,7 +13,7 @@ import (
 
 func main() {
 
-	fmt.Println("Brining Server online...")
+	fmt.Printf("Starting Server on port %v:%v", os.Getenv("APP_URL"), os.Getenv("APP_PORT"))
 
 	// Create Go Server
 	router := httprouter.New()
