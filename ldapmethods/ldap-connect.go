2497a8f5c48361136f57439373f7ab29f2ed5a4a
BillyPurvis <purvisbilly@outlook.com>
2018-10-26 15:09:14 +0100

    Add LDAP fields as map for key value data and implement pagination on LDAP request

--- ldapmethods/ldap-connect.go
+++ ldapmethods/ldap-connect.go
@@ -5,9 +5,11 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/BillyPurvis/boommessaging-go/uuid"
+	logrus "github.com/sirupsen/logrus"
 	ldap "gopkg.in/ldap.v2"
 )
+
+// Fields is a slice of maps where the key is the device var ID and key is the value
 
 // ConnectionDetails For LDAP
 type ConnectionDetails struct {
@@ -18,26 +20,27 @@ type ConnectionDetails struct {
 	BaseDN      string `json:"base_dn"`
 	Identifier  string
 	Password    string
-	Fields      []string               `json:"fields,omitempty"`
+	RequestID   string                 `json:"request_id"`
+	Fields      []map[string]string    `json:"fields,omitempty"`
 	QueryParams map[string]interface{} `json:"query_params,omitempty"`
-	Limit       string                 `json:"limit,omitempty"`
 }
 
 // LDAPConnectionBind Returns LDAP Connection Binding
-func LDAPConnectionBind(connectionDetails *ConnectionDetails) *ldap.Conn {
+func LDAPConnectionBind(connectionDetails *ConnectionDetails) (*ldap.Conn, error) {
 	// By default, Port 389 should support SSL. If not, the user can define port 636 which is SSL.
 	conn, err := ldap.Dial("tcp", fmt.Sprintf("%s:%v", connectionDetails.Host, connectionDetails.Port))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// Create LDAP Binding
 	err = conn.Bind(connectionDetails.Identifier, connectionDetails.Password)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+
 	// Return connection binding
-	return conn
+	return conn, nil
 }
 
 // Convert String to a unsigned 32 bit integer
@@ -61,62 +64,98 @@ func buildSearchTermsString(connectionDetails *ConnectionDetails) string {
 
 // GetEntries Return results from LDAP
 func GetEntries(connectionDetails *ConnectionDetails) ([]map[string]interface{}, error) {
-	conn := LDAPConnectionBind(connectionDetails)
+
+	conn, err := LDAPConnectionBind(connectionDetails)
+	if err != nil {
+		return nil, err
+	}
 	defer conn.Close()
 
 	searchQuery := buildSearchTermsString(connectionDetails)
-	pageSizeuint, err := convertStringToUint32(connectionDetails.Limit)
+	// pageSizeuint, err := convertStringToUint32(connectionDetails.Limit)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	pagingControl := ldap.NewControlPaging(1000)
 
-	if err != nil {
-		return nil, err
-	}
-	pagingControl := ldap.NewControlPaging(pageSizeuint)
+	var recordTotal int
+	for {
+		// Make Search Request defining base DN, attributes and filters
+		searchRequest := ldap.NewSearchRequest(
+			fmt.Sprintf("cn=%v,dc=%v,dc=com,dc=local", connectionDetails.CN, connectionDetails.BaseDN),
+			ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+			fmt.Sprintf("(&%v)", searchQuery),
+			[]string{},
+			[]ldap.Control{pagingControl},
+		)
 
-	// Make Search Request defining base DN, attributes and filters
-	searchRequest := ldap.NewSearchRequest(
-		fmt.Sprintf("cn=%v,dc=%v,dc=com,dc=local", connectionDetails.CN, connectionDetails.BaseDN),
-		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		fmt.Sprintf("(&%v)", searchQuery),
-		connectionDetails.Fields,
-		[]ldap.Control{pagingControl},
-	)
-
-	// Make Search Request
-	sr, err := conn.Search(searchRequest)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create list of maps
-	entryList := make([]map[string]interface{}, 0)
-	// Iterate through AD Records
-	for i, entry := range sr.Entries {
-
-		// Create new map item and append to list
-		entryList = append(entryList, make(map[string]interface{}))
-
-		// Iterate through requestedFields
-		for _, field := range connectionDetails.Fields {
-
-			fieldValue := entry.GetAttributeValue(field)
-
-			uuid := uuid.CreateUUID()
-			entryList[i]["uuid"] = uuid
-			// Check for empty fields and assign nil if empty
-			if fieldValue != "" {
-				entryList[i][field] = fieldValue
-			} else {
-				entryList[i][field] = nil
-			}
+		records, err := conn.Search(searchRequest)
+		if err != nil {
+			return nil, err
 		}
+
+		logrus.WithFields(logrus.Fields{
+			"LDAP AD Batch Count": len(records.Entries),
+		}).Info("LDAP Contacts batch count")
+
+		recordTotal += len(records.Entries)
+
+		// TODO: Add logic for saving and such
+		// TODO: Pass
+
+		// Loop through fields and map and store.
+		updatedControl := ldap.FindControl(records.Controls, ldap.ControlTypePaging)
+		if ctrl, ok := updatedControl.(*ldap.ControlPaging); ctrl != nil && ok && len(ctrl.Cookie) != 0 {
+			pagingControl.SetCookie(ctrl.Cookie)
+			continue
+		}
+
+		break
+
 	}
-	return entryList, nil
+
+	fmt.Println(recordTotal)
+	// // Make Search Request
+	// sr, err := conn.Search(searchRequest)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	//TODO: We don't need this, we just need to return a status
+	// // Create list of maps
+	// entryList := make([]map[string]interface{}, 0)
+	// // Iterate through AD Records
+	// for i, entry := range sr.Entries {
+
+	// 	// Create new map item and append to list
+	// 	entryList = append(entryList, make(map[string]interface{}))
+
+	// 	// Iterate through requestedFields
+	// 	for _, field := range connectionDetails.Fields {
+
+	// 		fieldValue := entry.GetAttributeValue(field)
+
+	// 		uuid := uuid.CreateUUID()
+	// 		entryList[i]["uuid"] = uuid
+	// 		// Check for empty fields and assign nil if empty
+	// 		if fieldValue != "" {
+	// 			entryList[i][field] = fieldValue
+	// 		} else {
+	// 			entryList[i][field] = nil
+	// 		}
+	// 	}
+	// }
+	m := make([]map[string]interface{}, 0)
+	return m, nil
 }
 
 // GetEntryAttributes Returns attribute field lists for an entry
 func GetEntryAttributes(connectionDetails *ConnectionDetails) ([]string, error) {
 
-	conn := LDAPConnectionBind(connectionDetails)
+	conn, err := LDAPConnectionBind(connectionDetails)
+	if err != nil {
+		return nil, err
+	}
 	defer conn.Close() // Defer until end of function
 
 	// Make Search request
